38fb9b2a5fa38e9271ae64f38828972dcb755284
gusameteo <cosmospiral.nvgvup.hddfim.go4@gmail.com>
2020-06-02 18:41:18 +0900

    Initial Commit

---
+++ Callme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Callme
+//
+//  Created by Yoshiaki Saegusa on 2020/06/02.
+//  Copyright © 2020 Yoshiaki Saegusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
